724338e667b89fad6479020319999e2c07303108
Kamil Niemczyk <kamil.niemczyk@capgemini.com>
2015-07-29 16:49:48 +0200

    Moved 3 first beans from XML to Annotations

---
+++ spring-data-jpa/src/main/java/pl/spring/demo/common/SequenceConfig.java
@@ -0,0 +1,9 @@
+package pl.spring.demo.common;
+
+//@Configuration
+public class SequenceConfig {
+//	@Bean
+	public Sequence sequence() {
+		return new Sequence();
+	}
+}
--- spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/BookServiceImpl.java
@@ -1,13 +1,16 @@
 package pl.spring.demo.service.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import pl.spring.demo.dao.BookDao;
 import pl.spring.demo.service.BookService;
 import pl.spring.demo.to.BookTo;
 
-import java.util.List;
-
+@Service
 public class BookServiceImpl implements BookService {
-
     private BookDao bookDao;
 
     @Override
@@ -30,6 +33,7 @@ public class BookServiceImpl implements BookService {
         return bookDao.save(book);
     }
 
+    @Autowired
     public void setBookDao(BookDao bookDao) {
         this.bookDao = bookDao;
     }
